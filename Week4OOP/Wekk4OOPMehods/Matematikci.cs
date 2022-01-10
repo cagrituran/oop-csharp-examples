6394ccb2a6ec767f910b1221da2203020294469d
cagrituran <cagri.turann@hotmail.com>
2022-01-10 15:47:32 +0300

    Week4
    OOPC#

---
+++ Week4OOP/Wekk4OOPMehods/Matematikci.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Wekk4OOPMehods
+{
+   
+    public class Matematikci
+    {
+
+
+        public Matematikci()
+        {
+            cnt++;
+            cnt2++;
+        }
+
+        public static int Faktoriyel(int n)
+        {
+            if (n == 0)
+                return 1;
+
+            return n * Faktoriyel(n - 1);
+
+        }
+
+        int a = 5;
+        public static int b = 5;
+
+        public static int cnt=0;
+        public int cnt2 = 0;
+
+
+        public int GetCnt()
+        {
+            return cnt;
+        }
+        public int GetCnt2()
+        {
+            return cnt2;
+        }
+
+
+    }
+}
